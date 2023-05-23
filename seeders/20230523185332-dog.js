efa6659c6c4805c06b343e718a9202ad1224edfb
Julian <julian.franco@icloud.com>
2023-05-23 14:56:38 -0400

    create dog seeder

---
+++ seeders/20230523185332-dog.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const date = new Date()
+
+    await queryInterface.bulkInsert('Dogs', [{ 
+      name: 'Seth',
+      breed: 'Poodle',
+      age: 4,
+      createdAt: date,
+      updatedAt: date,
+    }])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Dogs', null, {})
+  }
+};
